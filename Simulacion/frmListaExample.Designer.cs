ffc89597d0be1d6b7b8d6faa968107ecf86ea706
camiloreynaga <camilo.reynaga@gmail.com>
2015-12-14 14:52:32 -0500

    clase 6 - ejemplos de listas y uso de metodos comunes. Add, Find, FindIndex

---
+++ Simulacion/frmListaExample.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Simulacion
+{
+    partial class frmListaExample
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFind = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(12, 12);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(75, 23);
+            this.btnFind.TabIndex = 0;
+            this.btnFind.Text = "button1";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 72);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(306, 196);
+            this.dgv.TabIndex = 1;
+            // 
+            // frmListaExample
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 287);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnFind);
+            this.Name = "frmListaExample";
+            this.Text = "frmListaExample";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.DataGridView dgv;
+    }
+}
---
+++ Simulacion/frmListaExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulacion
+{
+    public partial class frmListaExample : Form
+    {
+        public frmListaExample()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            List<string> list1 = new List<string>();
+
+            list1.Add("Uno");
+            list1.Add("Dos");
+            list1.Add("Tres");
+            list1.Add("Cuatro");
+
+            /// ubicacion de indice en la lista
+            //var r = list1.FindIndex(x => x == "Uno").ToString();
+            
+            
+            //forma de buscar un elemento
+            //var r = list1.Find(x => x.Contains("o")).ToString();
+
+            //var r = list1.Contains("Uno").ToString();
+
+            var r = list1.Exists(x=>x =="uno").ToString();
+
+            list1.Contains<string>("uno");
+            // 
+            var aO = list1.Find(x=>x.Length >= 4).ToString();
+                MessageBox.Show(aO);
+
+            //dgv.DataSource = list1.ToArray<string>();
+
+        }
+    }
+}
